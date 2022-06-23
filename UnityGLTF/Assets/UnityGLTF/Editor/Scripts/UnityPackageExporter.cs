150ab3086c26f5db4796c594b32550c0c4242b6d
DuanLian <duanlian@chukong-inc.com>
2022-06-23 17:43:22 +0800

    FromdevToUGC: package导出工具

---
+++ UnityGLTF/Assets/UnityGLTF/Editor/Scripts/UnityPackageExporter.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Rathian
+{
+	public class UnityPackageExporter
+	{
+#if UNITY_EDITOR
+		[MenuItem("Tools/UnityPackageExporter/Export UnityGLTF")]
+		private static void ExportLobby()
+		{
+			var path = EditorUtility.SaveFilePanel("Save unitypackage", "", "UnityGLTF", "unitypackage");
+			if (path == "")
+			{
+				return;
+			}
+
+			string[] assetPaths = new string[]
+			{
+				"Assets/UnityGLTF/CKUnityGLTF.asmdef",
+				"Assets/UnityGLTF/Runtime",
+				"Assets/UnityGLTF/Scripts/ConfigJsonExtension",
+				"Assets/UnityGLTF/Scripts/MaterialExtension",
+				"Assets/UnityGLTF/Scripts/MeshFilterAndMeshColliderExtension",
+				"Assets/UnityGLTF/Scripts/Server",
+				"Assets/UnityGLTF/Scripts/IPropExtension.cs",
+				"Assets/UnityGLTF/Scripts/ModelExporter.cs",
+				"Assets/UnityGLTF/Scripts/ModelImporter.cs",
+				"Assets/UnityGLTF/Scripts/ModelLoader.cs",
+				"Assets/UnityGLTF/Scripts/ModelLoadSample.cs",
+				"Assets/UnityGLTF/Scripts/MyGLTFRoot.cs",
+				"Assets/UnityGLTF/Scripts/Utils.cs",
+				"Assets/UnityGLTF/Scripts/VirtualStreamLoader.cs",
+            };
+
+			//var allPaths = AssetDatabase.GetDependencies(assetPaths);
+
+			AssetDatabase.ExportPackage(assetPaths, path, ExportPackageOptions.Interactive | ExportPackageOptions.Recurse /*| ExportPackageOptions.IncludeDependencies*/);
+		}
+#endif
+	}
+}
+
